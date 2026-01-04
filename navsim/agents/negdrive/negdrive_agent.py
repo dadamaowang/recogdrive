1da9683947d07eebfd513547483a1f4997b4a1c3
Meronym <dadamaowang@yeah.net>
2026-01-04 10:35:42 +0800

    update

---
+++ navsim/agents/negdrive/negdrive_agent.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   negdrive_agent.py
+@Time    :   2026/01/04 10:32:54
+@Author  :   Nuoqian Xiao
+@Version :   0.0.1
+@Contact :   feimaoxiaotianshi@outlook.com
+@License :   (C)Copyright 2024-2025, Nuoqian Xiao
+@Status  :   None
+@Desc    :   None
+'''
+
