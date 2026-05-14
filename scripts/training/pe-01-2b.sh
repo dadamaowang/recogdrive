ceb4d4f56fddb35fd3936dc29a47ef6b38c53598
Meronym <dadamaowang@yeah.net>
2026-05-14 21:04:44 +0800

    update

--- scripts/training/pe-01-2b.sh
+++ scripts/training/pe-01-2b.sh
@@ -11,12 +11,12 @@ export NUPLAN_MAP_VERSION="nuplan-maps-v1.0"
 # export CACHE_PATH=null
 
 TRAIN_TEST_SPLIT=navtrain   
-EXP_NAME=debug
-FAST_DEV_RUN=true
+EXP_NAME=pre_2b_latent_sta_0511
+FAST_DEV_RUN=false
 
 export MASTER_PORT=63669
 export PORT=63665
-export GPUS=2
+export GPUS=4
 export GPUS_PER_NODE=$GPUS
 
 MASTER_PORT=${MASTER_PORT:-63669}
